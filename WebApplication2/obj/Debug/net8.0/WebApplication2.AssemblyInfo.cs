140afeb8bc01abb0db5800e294d84343a4b63bb7
Vu Minh Quy <118278609+viciousircle@users.noreply.github.com>
2024-11-29 00:07:13 +0700

    Update filter for choose

--- WebApplication2/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+514ee685da9c97d3d8acd9451065f8700f4b9a6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+035717b92a706f789f16d92edee8d9b485a27ab3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
