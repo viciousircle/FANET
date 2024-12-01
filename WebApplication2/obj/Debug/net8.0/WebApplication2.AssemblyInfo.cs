863a5e9e88295c776f622772d0cf0750c773da22
hoangnamgithub <nguyenhoangnam0437@gmail.com>
2024-12-01 22:49:58 +0700

    dc k

--- WebApplication2/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca7ce64594f3191cfe286af42547385483cb18ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4fff1719d5cc2773ea52b8cc3b99f4763931878")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
