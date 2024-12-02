517701dfabcb263fb26fec3900aae11adb0aedd4
Vu Minh Quy <118278609+viciousircle@users.noreply.github.com>
2024-12-02 20:05:10 +0700

    Nothing

--- WebApplication2/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59294e60debeb77ce38f13554ff9eb5b5ba0b860")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5369cd2f07492ec3edc675f3aca02a307866f20")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
